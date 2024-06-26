76206acfcfd67dc9a20e864f4ffd8e9e25310ec3
Jupi <spacejupiter0@gmail.com>
2024-06-26 13:37:44 +0300

    project init

--- src/API/client.ts
+++ src/API/client.ts
@@ -2,9 +2,11 @@ import axios from "axios";
 
 const baseUrl = import.meta.env.VITE_API_URL;
 
-export default axios.create({
+const axiosClient = axios.create({
   baseURL: `${baseUrl}`,
   headers: {
     "Content-type": "application/json",
   },
 });
+
+export const fetcher = (url: string) => axiosClient.get(url).then(res => res.data);
